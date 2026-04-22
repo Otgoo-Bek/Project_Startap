94bfe21392c66566244b896ed417c0baf35665c5
Otgoo-Bek <otgontsetsegbeykbolot@gmail.com>
2026-04-22 18:53:02 +1000

    fix: add documentRoutes and reviewRoutes to app.ts

--- src/routes/document.routes.ts
+++ src/routes/document.routes.ts
@@ -1,10 +1,10 @@
-import { Router } from 'express';
-import { getDocs, createDoc, deleteDoc } from '../controllers/document.controller';
+import { Router, RequestHandler } from 'express';
+import { getDocs, createDoc, deleteDoc, upload } from '../controllers/document.controller';
 
 const router = Router();
 
 router.get('/documents/:userId', getDocs);
-router.post('/documents', createDoc);
 router.delete('/documents/:id', deleteDoc);
+router.post('/documents', upload.single('file') as RequestHandler, createDoc);
 
 export default router;
