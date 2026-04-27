fa77bdc2298081f31c0e97fc34ec26dc94a81c5c
Otgoo-Bek <otgontsetsegbeykbolot@gmail.com>
2026-04-27 17:19:30 +1000

    feat: multiple reviews per shift + delete review

--- src/routes/review.routes.ts
+++ src/routes/review.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { createReview, getReviews } from '../controllers/review.controller';
+import { createReview, getReviews, deleteReview } from '../controllers/review.controller';
 
 const router = Router();
 
 router.post('/reviews', createReview);
 router.get('/reviews/:userId', getReviews);
+router.delete('/reviews/:id', deleteReview);
 
 export default router;
