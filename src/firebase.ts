74202d933561a43788c9f48674bc56d7e03cd03f
Otgoo-Bek <otgontsetsegbeykbolot@gmail.com>
2026-05-10 22:50:55 +1000

    fix: read serviceAccount from Render secret file

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,15 @@
 import * as admin from 'firebase-admin';
-import * as serviceAccount from '../serviceAccount.json';
+import * as fs from 'fs';
+import * as path from 'path';
 
 if (!admin.apps.length) {
+  // На Render файл лежит в /etc/secrets/
+  const secretPath = '/etc/secrets/serviceAccount.json';
+  const localPath = path.join(__dirname, '../../serviceAccount.json');
+  
+  const filePath = fs.existsSync(secretPath) ? secretPath : localPath;
+  const serviceAccount = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
   });
